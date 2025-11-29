2e2cfdff107983b3c5254ba1c4f62bd124c1960c
yangying0712 <all27allhz@qq.com>
2025-11-29 22:06:36 +0800

    08-后端Web实战(员工管理)完成

--- this-web-management/src/test/java/org/xd/TliasWebManagementApplicationTests.java
+++ this-web-management/src/test/java/org/xd/TliasWebManagementApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.xd.mapper.EmpMapper;
-import org.xd.pojo.Emp;
-
-import java.util.List;
 
 @SpringBootTest
 class TliasWebManagementApplicationTests {
@@ -16,7 +13,7 @@ class TliasWebManagementApplicationTests {
 
     @Test
     public void testList(){
-        List<Emp> empList = empMapper.list(0, 10);
-        empList.forEach(System.out::println);
+//        List<Emp> empList = empMapper.list(0, 10);
+//        empList.forEach(System.out::println);
     }
 }
